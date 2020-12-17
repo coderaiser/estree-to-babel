a13c80ec321d09f2cacea2243c149161e26a03db
coderaiser <coderaiser@cloudcmd.io>
2020-12-17 13:49:54 +0200

    test(set-literal) coverage

--- .madrun.js
+++ .madrun.js
@@ -3,7 +3,7 @@
 const {run} = require('madrun');
 
 module.exports = {
-    'test': () => `tape 'test/*.js'`,
+    'test': () => `tape 'test/*.js' 'lib/**/*.spec.js'`,
     'watch:test': async () => `nodemon -w lib -w test -x ${await run('test')}`,
     'lint': () => `putout .`,
     'fix:lint': () => run('lint', '--fix'),
