853e82b64894337c194579defff43a8675534086
coderaiser <coderaiser@cloudcmd.io>
2021-03-01 22:29:49 +0200

    chore(madrun) lint

--- .madrun.mjs
+++ .madrun.mjs
@@ -1,16 +1,14 @@
-'use strict';
+import {run} from 'madrun';
 
-const {run} = require('madrun');
-
-module.exports = {
+export default {
     'test': () => `tape 'test/*.js' 'lib/**/*.spec.js'`,
     'watch:test': async () => `nodemon -w lib -w test -x ${await run('test')}`,
     'lint': () => `putout .`,
     'fresh:lint': () => run('lint', '--fresh'),
     'lint:fresh': () => run('lint', '--fresh'),
     'fix:lint': () => run('lint', '--fix'),
-    'coverage': async () => `nyc ${await run('test')}`,
-    'report': () => `nyc report --reporter=text-lcov | coveralls || true`,
+    'coverage': async () => `c8 ${await run('test')}`,
+    'report': () => 'c8 report --reporter=lcov',
     'fixture': async () => `UPDATE_FIXTURE=1 ${await run('test')}`,
 };
 
