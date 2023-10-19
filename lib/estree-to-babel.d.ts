41ec8c385f0dccc4353764910df9a18e53f6d507
coderaiser <coderaiser@cloudcmd.io>
2023-10-19 16:20:00 +0300

    fix: types: drop comment

--- lib/estree-to-babel.d.ts
+++ lib/estree-to-babel.d.ts
@@ -3,10 +3,4 @@ import {
     types,
 } from '@putout/babel';
 
-/**
- * Convert an SWC ast to a babel ast
- * @param ast {Module} SWC ast
- * @param {string} [src=""] Source code
- * @returns {ParseResult<File>} Babel ast
- */
 export default function toBabel(ast: types.Node, src?: string): ParseResult<types.File>;
