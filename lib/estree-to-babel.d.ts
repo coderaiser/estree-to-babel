c87781f610a5d939be754ca52900c812cb050245
remcohaszing <remcohaszing@users.noreply.github.com>
2025-04-07 08:53:14 +0000

    chore: estree-to-babel: actions: lint ☘️

--- lib/estree-to-babel.d.ts
+++ lib/estree-to-babel.d.ts
@@ -1,3 +1,4 @@
 import {ParseResult, types} from '@putout/babel';
 
 export function toBabel(ast: types.Node, src?: string): ParseResult<types.File>;
+
--- test/estree-to-babel.errors.ts
+++ test/estree-to-babel.errors.ts
@@ -1,4 +1,5 @@
 import {toBabel} from '..';
+
 // THROWS Expected 1-2 arguments, but got 0.
 toBabel();
 // THROWS Argument of type 'number' is not assignable to parameter of type 'Node'.
