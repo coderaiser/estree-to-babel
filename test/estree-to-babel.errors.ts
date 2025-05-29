d749b236bc13772b135c285b7cd63aa2390ca801
coderaiser <coderaiser@users.noreply.github.com>
2025-05-29 19:17:35 +0000

    chore: estree-to-babel: actions: lint ☘️

--- test/estree-to-babel.errors.ts
+++ test/estree-to-babel.errors.ts
@@ -1,6 +1,5 @@
 import {estreeToBabel} from '..';
 
-// THROWS Expected 1-2 arguments, but got 0.
 estreeToBabel();
 // THROWS Argument of type 'number' is not assignable to parameter of type 'Node'.
 estreeToBabel(1);
